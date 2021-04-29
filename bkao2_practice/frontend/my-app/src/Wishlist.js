0505fea98bcbc67c85f161b41b0e420b2146f168
SpicyOwl <duanxu@bc.edu>
2021-04-28 23:59:15 -0400

    delete button will update the webpage now

--- bkao2_practice/frontend/my-app/src/Wishlist.js
+++ bkao2_practice/frontend/my-app/src/Wishlist.js
@@ -6,8 +6,10 @@ import { useAuth } from "./App.js";
 
 function Wishlist(){
     let auth = useAuth();
+    const [deletsql, setdeletsql] = useState('');
     
     const [wishlistitem, setwishlistitem] = useState([]);
+
     useEffect(() => {
 
         let sql = "SELECT * FROM Contains WHERE wishlist_id=" + auth.user.wishlist_id;
@@ -20,20 +22,31 @@ function Wishlist(){
             (response.data).forEach((val,ind) => {
                 console.log("val is: " + val.product_id);
                 setwishlistitem(wishlistitem=>[...wishlistitem, val]);
-
-             
             })
         })
+
+
+
+
+        
     },[auth.user])
 
 
     const deleteFromContain = (wishlist_id, product_id) =>{
+        
         let sql = "DELETE FROM Contains WHERE wishlist_id =" + wishlist_id +" AND product_id = \""+product_id +"\"";
+        setdeletsql(product_id);
         console.log(sql);
         Axios.put(`http://localhost:3002/api/deleteContains`,{       
                     sql:sql
         });
-    }
+        alert(deletsql + "deleted successfully!");
+
+        
+        setwishlistitem(wishlistitem.filter(val => val.product_id !== product_id));
+
+    };
+
     return (
         <div className = "App">
         <h1>wishlist page</h1>
@@ -45,7 +58,7 @@ function Wishlist(){
             // console.log(val);
             return(
                 <div className = "card">
-                    <p>Wishlist id:{val.wishlist_id} </p>
+                    {/* <p>Wishlist id:{val.wishlist_id} </p> */}
                       <p> Wishlist Item: {val.product_id} </p>
                       <p>  add_date:{val.add_date} </p>
                       <p> priority: {val.priority} </p>
@@ -55,11 +68,7 @@ function Wishlist(){
                 </div>
 
             );
-        }
-
-        )
-
-        }
+        })}
         </div>
     );
 }
