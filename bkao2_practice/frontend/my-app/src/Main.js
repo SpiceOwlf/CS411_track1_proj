956d5c77074b8ed9bb4ddad072b875c306ae84fc
JackZheng <47945294+jzheng19961104@users.noreply.github.com>
2021-04-28 23:09:39 -0500

    Update Main.js
    Merge conflicts change

--- bkao2_practice/frontend/my-app/src/Main.js
+++ bkao2_practice/frontend/my-app/src/Main.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Userinfo from './Userinfo.js';
 import Notification from './Notification.js'
 import Home from './Home.js';
+import Wishlist from './Wishlist.js';
 
 const Main = () => {
   return (
@@ -10,8 +11,10 @@ const Main = () => {
       <Route exact path='/' component={Home}></Route>
       <Route exact path='/notification' component={Notification}></Route>
       <Route exact path='/userinfo' component={Userinfo}></Route>
+      <Route exact path='/wishlist' component={Wishlist}></Route>
     </Switch>
   );
 }
+
 
 export default Main;
